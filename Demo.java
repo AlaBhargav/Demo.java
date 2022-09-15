7fdbca47981fa4c0da1846244a8a8ed1e991bff3
AlaBhargav <113082018+AlaBhargav@users.noreply.github.com>
2022-09-15 09:17:32 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,25 @@
+import java.util.*
+class Demo
+{
+    public static void main(String arg[])
+    {
+      scanner sc=new scanner(System.in);
+      system.out.println("Enter name:");
+      string name=sc.nextline();
+      System.out.println("Enter name:");
+      int number=sc.nextInt();
+      System.out.println("Enter salary:");
+      double salary=sc.nextDoble();
+
+
+
+
+
+
+
+
+
+    
+    
+
+    }
